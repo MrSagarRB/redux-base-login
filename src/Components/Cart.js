e3d5cd1e53adc35bf5437e0b785fcde1eb6a3c22
Sagar Borude <87903581+MrSagarRB@users.noreply.github.com>
2022-06-27 19:00:12 +0530

    Total Amt Feature

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -5,10 +5,27 @@ import { ContextProvider } from "../Context";
 
 const Cart = () => {
   let { cart, setCart } = useContext(ContextProvider);
+  let [amt,setAmt]= useState();
+
+
+
+  useEffect(()=>{
+    setAmt(cart.reduce((acc,curr)=>acc + Number(curr.price),0));
+  },[cart])
+
+  console.log(amt);
+
 
   return (
-    <div className=" ">
-      <div className=" h-[90%]  grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-10 px-[50px] pt-[10px] place-items-center overflow-scroll ">
+    <div className=" h-screen">
+      <div className="p-10">
+        <p className="text-2xl">Your Cart </p>
+        <p className="text-xl">
+          Total item : <span> {cart.length}</span>{" "}
+        </p>
+        <p> Total Ammount:  {amt} Rs.</p>
+      </div>
+      <div className=" h-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-10 px-[50px] pt-[10px] place-items-center overflow-y-scroll ">
         {cart.map((prod) => {
           return (
             <SingleProduct
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -16,7 +16,7 @@ const Header = () => {
           <li>
             <Link to="/">Home</Link>
           </li>
-         
+
           <li>
             <Link to="/cart">
               Cart <span> ({cart.length})</span>{" "}
